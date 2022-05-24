d934fd9c99a3e5051bbf58bac7257c903d1a264e
liwei <liweilijie@gmail.com>
2022-05-24 18:01:24 +0800

    develop...

--- src/views/his/medicinal/index.vue
+++ src/views/his/medicinal/index.vue
@@ -15,18 +15,14 @@
           @keyup.enter="getList"
         />
       </el-form-item>
-      <el-form-item label="药箱" prop="category_name">
-        <el-select
-            v-model="queryParams.category_name"
-            placeholder="药箱名称"
-            clearable
-        >
+      <el-form-item label="药箱" prop="category_id">
+        <el-select v-model="queryParams.category_id" placeholder="请选择药箱">
           <el-option
-              v-for="dict in listCategory"
-              :key="dict.value"
-              :label="dict.label"
-              :value="dict.value"
-          />
+              v-for="item in cateOptions"
+              :key="item.id"
+              :label="item.name"
+              :value="item.id"
+          ></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="状态" prop="status">
@@ -82,6 +78,16 @@
           @click="handleDelete"
           v-hasPermi="['his/medicinal/delete']"
           >删除</el-button
+        >
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="info"
+          plain
+          icon="Upload"
+          @click="handleImport"
+          v-hasPermi="['his/medicinal/import']"
+          >导入</el-button
         >
       </el-col>
       <el-col :span="1.5">
@@ -171,8 +177,15 @@
         <el-form-item label="药品名称" prop="name">
           <el-input v-model="form.name" placeholder="请输入药品名称" />
         </el-form-item>
-        <el-form-item label="药箱" prop="category_name">
-          <el-input v-model="form.category_name" placeholder="请输入药箱" />
+        <el-form-item label="药箱" prop="category_id">
+          <el-select v-model="form.category_id" placeholder="请选择药箱">
+            <el-option
+                v-for="item in cateOptions"
+                :key="item.id"
+                :label="item.name"
+                :value="item.id"
+            ></el-option>
+          </el-select>
         </el-form-item>
         <el-form-item label="药品编码" prop="batch_number">
           <el-input v-model="form.batch_number" placeholder="请输入编码名称" />
@@ -201,11 +214,58 @@
         </div>
       </template>
     </el-dialog>
+
+    <!-- 用户导入对话框 -->
+    <el-dialog
+      :title="upload.title"
+      v-model="upload.open"
+      width="400px"
+      append-to-body
+    >
+      <el-upload
+        ref="uploadRef"
+        :limit="1"
+        accept=".xlsx, .xls"
+        :headers="upload.headers"
+        :action="upload.url + '?updateSupport=' + upload.updateSupport"
+        :disabled="upload.isUploading"
+        :on-progress="handleFileUploadProgress"
+        :on-success="handleFileSuccess"
+        :auto-upload="false"
+        drag
+      >
+        <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+        <template #tip>
+          <div class="el-upload__tip text-center">
+            <div class="el-upload__tip">
+              <el-checkbox
+                v-model="upload.updateSupport"
+              />是否更新已经存在的用户数据
+            </div>
+            <span>仅允许导入xls、xlsx格式文件。</span>
+            <el-link
+              type="primary"
+              :underline="false"
+              style="font-size: 12px; vertical-align: baseline"
+              @click="importTemplate"
+              >下载模板</el-link
+            >
+          </div>
+        </template>
+      </el-upload>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button type="primary" @click="submitFileForm">确 定</el-button>
+          <el-button @click="upload.open = false">取 消</el-button>
+        </div>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
 <script setup name="Post">
-import { getCurrentInstance,ref,toRefs,reactive } from 'vue';
+import { onMounted, getCurrentInstance,ref,toRefs,reactive } from 'vue';
 import {
   listMedicinal,
   addMedicinal,
@@ -228,12 +288,29 @@ const single = ref(true);
 const multiple = ref(true);
 const total = ref(0);
 const title = ref('');
+const cateOptions = ref([]);
+
+/*** 用户导入参数 */
+const upload = reactive({
+  // 是否显示弹出层（用户导入）
+  open: false,
+  // 弹出层标题（用户导入）
+  title: '',
+  // 是否禁用上传
+  isUploading: false,
+  // 是否更新已经存在的用户数据
+  updateSupport: 0,
+  // 设置上传的请求头部
+  headers: { Authorization: 'Bearer ' + getToken() },
+  // 上传的地址
+  url: import.meta.env.VITE_APP_BASE_API + 'his/medicinal/import',
+});
 
 const data = reactive({
   form: {},
   queryParams: {
     page_num: 1,
-    page_size: 10,
+    page_size: 3,
     post_code: undefined,
     post_name: undefined,
     status: undefined,
@@ -252,6 +329,25 @@ const data = reactive({
 });
 
 const { queryParams, form, rules } = toRefs(data);
+
+onMounted(() => {
+  get_options();
+})
+
+// 获取选项
+async function get_options() {
+  let queryParams = {
+    page_num: 1,
+    page_size: Number.MAX_SAFE_INTEGER,
+  };
+  const [{ list: cates}] = await Promise.all(
+      [listCategory(queryParams)]
+  );
+  // const cates = await listCategory(queryParams.value);
+  cateOptions.value = cates;
+  console.log('cates:', cates);
+  console.log('options:', cateOptions);
+}
 
 /** 查询药品列表 */
 async function getList() {
@@ -307,6 +403,7 @@ async function handleUpdate(row) {
   form.value = response;
   open.value = true;
   title.value = '修改药品';
+  get_options();
 }
 /** 提交按钮 */
 function submitForm() {
@@ -352,6 +449,43 @@ function handleExport() {
     },
     `post_${new Date().getTime()}.xlsx`
   );
+}
+
+/** 导入按钮操作 */
+function handleImport() {
+  upload.title = '用户导入';
+  upload.open = true;
+}
+
+/** 下载模板操作 */
+function importTemplate() {
+  proxy.download(
+    'system/user/importTemplate',
+    {},
+    `user_template_${new Date().getTime()}.xlsx`
+  );
+}
+/**文件上传中处理 */
+const handleFileUploadProgress = (event, file, fileList) => {
+  upload.isUploading = true;
+};
+/** 文件上传成功处理 */
+const handleFileSuccess = (response, file, fileList) => {
+  upload.open = false;
+  upload.isUploading = false;
+  proxy.$refs['uploadRef'].clearFiles();
+  proxy.$alert(
+    "<div style='overflow: auto;overflow-x: hidden;max-height: 70vh;padding: 10px 20px 0;'>" +
+      response.msg +
+      '</div>',
+    '导入结果',
+    { dangerouslyUseHTMLString: true }
+  );
+  getList();
+};
+/** 提交上传文件 */
+function submitFileForm() {
+  proxy.$refs['uploadRef'].submit();
 }
 
 getList();
